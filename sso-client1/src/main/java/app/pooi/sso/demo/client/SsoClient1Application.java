49a04bdd54c0e5bc17db3152efdf240ebaf48dc7
sage417 <sage417@foxmail.com>
2018-10-02 12:36:06 +0800

    sso oauth client

--- sso-client1/src/main/java/app/pooi/sso/demo/client/SsoClient1Application.java
+++ sso-client1/src/main/java/app/pooi/sso/demo/client/SsoClient1Application.java
@@ -1,21 +1,48 @@
 package app.pooi.sso.demo.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.Filter;
 import java.security.Principal;
+import java.util.Base64;
+import java.util.Map;
 
 
 @SpringBootApplication
 @RestController
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class SsoClient1Application extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
 
     @GetMapping("/user")
     public Principal user(Principal principal) {
@@ -36,7 +63,82 @@ public class SsoClient1Application extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().addFilterBefore(ssoFilter(jwtTokenServices(jwtTokenStore())), BasicAuthenticationFilter.class);
+    }
+
+    public Filter ssoFilter(ResourceServerTokenServices tokenServices) {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/pooi");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(tokenServices);
+        return facebookFilter;
+    }
+
+    @Bean
+    @ConfigurationProperties("pooi.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("pooi.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties(this.facebook().getClientId(), this.facebook().getClientSecret());
+    }
+
+    @Bean
+    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+
+    @Bean
+    public DefaultTokenServices jwtTokenServices(TokenStore jwtTokenStore) {
+        DefaultTokenServices services = new DefaultTokenServices();
+        services.setTokenStore(jwtTokenStore);
+        return services;
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        String keyValue = facebookResource().getJwt().getKeyValue();
+        if (!StringUtils.hasText(keyValue)) {
+            keyValue = getKeyFromServer();
+        }
+        if (StringUtils.hasText(keyValue) && !keyValue.startsWith("-----BEGIN")) {
+            converter.setSigningKey(keyValue);
+        }
+        if (keyValue != null) {
+            converter.setVerifierKey(keyValue);
+        }
+        return converter;
+    }
+
+    private String getKeyFromServer() {
+        RestTemplate keyUriRestTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        String username = facebookResource().getClientId();
+        String password = facebookResource().getClientSecret();
+        if (username != null && password != null) {
+            byte[] token = Base64.getEncoder()
+                    .encode((username + ":" + password).getBytes());
+            headers.add("Authorization", "Basic " + new String(token));
+        }
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        String url = facebookResource().getJwt().getKeyUri();
+        return (String) keyUriRestTemplate
+                .exchange(url, HttpMethod.GET, request, Map.class).getBody()
+                .get("value");
     }
 
 }
