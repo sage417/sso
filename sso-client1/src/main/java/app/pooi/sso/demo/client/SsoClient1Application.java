39fc9a50f2284bec65dfc7db7ad5d688b45708ba
sage417 <sage417@foxmail.com>
2018-10-02 12:01:32 +0800

    sso oauth client

---
+++ sso-client1/src/main/java/app/pooi/sso/demo/client/SsoClient1Application.java
@@ -0,0 +1,42 @@
+package app.pooi.sso.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SsoClient1Application extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClient1Application.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+}
